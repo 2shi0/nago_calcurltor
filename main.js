f9f977445ba054a504b3450edb52a3480d30347c
Tsushima <b1019217@fun.ac.jp>
2023-12-27 18:21:58 +0900

    Net Blood Gain実装

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-const url = "blood.json";    // JSONファイル名
-let result;
+const url = "blood.json";
+let results;
+let command_name_array;
 
-// 起動時の処理
 window.addEventListener("load", () => {
     fetch(url)
         .then(response => {
             return response.json();
         })
         .then(json_data => {
-            result = json_data;
+            results = json_data;
+            command_name_array = Object.keys(results);
         });
 });
-
 
 function on_calc_button_click() {
 
     let textbox_element = document.getElementById("combo_text_box");
-    max_blood_gain = document.getElementById("max");
-    max_blood_gain.innerHTML = textbox_element.value;
-    net_blood_gain = document.getElementById("net");
-    net_blood_gain.innerHTML = textbox_element.value;
 
-    console.log(result["236K"]);
+    var net_value = 0;
+
+    for (var i = 0; i < command_name_array.length; i++) {
+        var str = String(textbox_element.value);
+        var target_str = String(command_name_array[i])
+        var count = (str.match(new RegExp(target_str, "g")) || []).length;
+        //console.log(target_str + ":" + String(count));
+        net_value += Number(results[target_str])*count
+    }
+
+    net_blood_gain = document.getElementById("net");
+    net_blood_gain.innerHTML = net_value;
+
 }
